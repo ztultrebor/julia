1d328fee1ff9ef4b8a9247adb9f41a437fbd981c
Elliot Saba <staticfloat@gmail.com>
2020-03-23 00:34:17 -0700

    Fix `libm` quoting

--- base/math.jl
+++ base/math.jl
@@ -561,8 +561,8 @@ Stacktrace:
 log1p(x)
 for f in (:log2, :log10)
     @eval begin
-        @inline ($f)(x::Float64) = nan_dom_err(ccall(($(string(f)), libm), Float64, (Float64,), x), x)
-        @inline ($f)(x::Float32) = nan_dom_err(ccall(($(string(f, "f")), libm), Float32, (Float32,), x), x)
+        @inline ($f)(x::Float64) = nan_dom_err(ccall(($(string(f)), $(libm)), Float64, (Float64,), x), x)
+        @inline ($f)(x::Float32) = nan_dom_err(ccall(($(string(f, "f")), $(libm)), Float32, (Float32,), x), x)
         @inline ($f)(x::Real) = ($f)(float(x))
     end
 end
